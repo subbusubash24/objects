54b7e256814adbfc8f0d74a25460f856e77c4953
subbusubash24 <107912022+subbusubash24@users.noreply.github.com>
2022-09-09 02:05:23 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,209 @@
+
+// function convert(value){
+//     if(value=="I"){
+//         return "1";
+//     }else if(value=="II"){
+//         return "2";
+//     }else{
+//         return false;
+//     }
+// }
+// let x=convert("III");
+// console.log(x);
+
+// var arr=["I","II","III","IV","V","VI","VII"];
+// function num(value){
+//       return arr[value-1];
+// }
+// let y=num(1);
+// console.log(y);
+
+
+
+
+// var rom={
+//     1:"I",
+//     2:"II",
+//     3:"III",
+//     4:"IV",
+//     5:"V",
+// }
+// function roman(value){
+//     if(value==1){
+//         return"I";
+//     }else if(value==2){
+//         return "II";
+//     }else if(value==3){
+//         return "III";
+//     }else if(value==4){
+//         return "IV";
+//     }else if(value==5){
+//         return 5;
+//     }else{
+//         return false;
+//     }
+
+// }
+// let x=roman(6)
+// console.log(x);
+
+
+// function add(value1,value2){
+//     let result=value1+value2;
+//     return result;
+// }
+// let x=add(2,7);
+// console.log(x);
+
+
+// function sub(sub1,sub2){
+//     let out=(sub1-sub2);
+//     return out;
+// }
+// let y=sub(6,3);
+// console.log(y);
+
+// function mul(mul1,mul2){
+//     let out1=mul1*mul2;
+//     return out1;
+// }
+// let d=mul(8,5);
+// console.log(d);
+
+// function arithmetic(value1,value2){
+//     var s={
+//         add:add(value1,value2),
+//         sub:sub(value1,value2),
+//         mul:mul(value1,value2)
+//     }
+// }
+
+
+
+
+// var car={
+//     name:"audi",
+//     color:"red",
+//     look:"attractive"
+// }
+// console.log(car);
+
+// function intro(value1,value2){
+//     return("my car name is "+value1+" and it looks "+value2);
+
+// }
+// let r=intro("bullero","nice");
+// console.log(r);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
